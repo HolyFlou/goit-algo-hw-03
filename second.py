be51d653b9ae60e926111046a5ee3d93eaaf430d
HolyFlou <stepanenko.nekita2020@gmail.com>
2025-11-03 11:41:04 +0200

    dz modified 2

--- second.py
+++ second.py
@@ -1,19 +1,10 @@
-from random import randint
+from random import sample
 
 def get_numbers_ticket(min, max, quantity):
-    if  min < 1 or max > 1000 or min > max or not (min < quantity < max): # Перевірка на обмеження
+    if min < 1 or max > 1000 or min > max or max-min < quantity: # Перевірка на обмеження
         return []
-    nums = []
-    randomnum = randint(min, max)
-    nums.append(randomnum)
-    for i in range(quantity-1):
-        randomnum = randint(min, max)
-        for num in nums: # Перевірка на унікальність чисел
-            if randomnum == num:
-                while randomnum == num:
-                    randomnum = randint(min, max)
-        nums.append(randomnum)
-    nums.sort() # Сортування списку
-    return nums
+    ticket = sample(range(min, max), quantity)
+    ticket.sort()
+    return ticket 
 
-print("Ваші лотерейні числа:", get_numbers_ticket(10, 14, 6))
+print("Ваші лотерейні числа:", get_numbers_ticket(1000, 1200, 3))
